d51e164dc81dd608c1caa563abfed7710a000938
Alexander <sharrikk@gmail.com>
2015-02-14 09:37:04 +0300

    init

---
+++ HW_SlicedImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW_SlicedImages
+//
+//  Created by Alexander on 14.02.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW_SlicedImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW_SlicedImages
+//
+//  Created by Alexander on 14.02.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
