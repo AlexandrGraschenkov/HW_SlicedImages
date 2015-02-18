f2a99e6a496216fe994d8d083189e993dfe09907
itisioslab <itisioslab@Mac-mini-Administrator.local>
2015-02-18 15:56:38 +0300

    add parsing

---
+++ HW_SlicedImages/NetManager.h
@@ -0,0 +1,13 @@
+//
+//  NetManager.h
+//  HW_SlicedImages
+//
+//  Created by itisioslab on 18.02.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NetManager : UITableViewCell
+
+@end
---
+++ HW_SlicedImages/NetManager.m
@@ -0,0 +1,40 @@
+//
+//  NetManager.m
+//  HW_SlicedImages
+//
+//  Created by itisioslab on 18.02.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import "NetManager.h"
+
+@implementation NetManager
+
++(instancetype)sharedInstance{
+    static id _singleton = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _singleton = [[self alloc] init];
+    });
+    return _singleton;
+}
+
+
+-(void)getListImages:(void(^)(NSArray *, NSError *))
+completion{
+    NSURL *url = [NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/55523423/NetExample/ListImages.json"];
+    [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse * response, NSError *error) {
+        if(!error){
+        NSArray *objects = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+            
+            if(!error){
+                completion(objects,nil);
+                return;
+            }
+        }
+        completion(nil,error);
+        }];
+        
+}
+
+@end
